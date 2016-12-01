5fb11fd460122e83542d82e09999656f8baf1a14
Antazx <guillermoantataz@gmail.com>
2016-12-01 10:35:23 +0100

    Comentarios añadidos, todo funciona correctamente

--- PracticaAoc.s
+++ PracticaAoc.s
@@ -10,14 +10,8 @@ Emes:				.asciiz "\nEl mes introducido es incorrecto.\n"
 Edia:				.asciiz "\nEl dia introducido es incorrecto.\n"
 Eano:				.asciiz "\nEl anyo introducido es incorrecto.\n"
 Eca:				.asciiz "\nSe ha encontrado un caracter incorrecto\n"
-Err1: 				.asciiz "\nAlgo ha salido regular\n"
-Ealg:  				.asciiz "\nHa habido un problema tecnico con el Algoritmo. Estamos trabajando en ello\n"
 
 cadena: 			.space 11 #Fecha leido
-
-D: 				.space 2
-M:				.space 2
-Y:				.space 4
 
 Semana:
 				.asciiz "Domingo "
@@ -75,7 +69,10 @@ __main:
 				syscall
 
 				la $s0, cadena			#Inicializamos puntero a la cadena
-
+				
+				la $a0, B			#Imprimimos salto de linea
+				li $v0, 4
+				syscall 
 
 				jal Leer			#Llamamos a la funcion para leer la cadena
 
@@ -84,113 +81,101 @@ __main:
 				beq $v0, -3, ErrA		#Error anyo
 				beq $v0, -4, ErrC		#Error Caracter
 				beq $v0, -5  ErrF		#Error Formato
-
-				bltz $v0, ErrAlg
-				bgt  $v0, 7, Erraaaa
-					
+				
+				add $s1, $s1, $v0
+				
 				la $a0, B			#Imprimimos salto de linea
 				li $v0, 4
 				syscall 
 						
-				la $a0, Semana			#Imprimimos dia de la semana
-				mul $t4, $v0, 11
-				add $a0, $a0, $t4
+				la $a0, Semana			#Cargamos la direccion de los dias de la semana
+				mul $t4, $s1, 11		#Calculamos el desplazamiento
+				add $a0, $a0, $t4		#Sumamos el desplazamiento a la direcion
 
-				li $v0, 4
+				li $v0, 4			#Imprimimos el dia de la semana
 				syscall
 
-				li $a0, 0
+				li $a0, 0			
 				add $a0, $a0, $t1
-				li $v0, 1
+				li $v0, 1			#Imprimimos el numero del dia
 				syscall
 
-				la $a0, Meses
-				addi $t2, $t2, -1
-				mul $t2, $t2, 20
-				add $a0, $a0, $t2
+				la $a0, Meses			#Cargamos la direccion de los meses
+				addi $t2, $t2, -1		#Restamos 1 a el numero del mes
+				mul $t2, $t2, 20		#Calculamos el desplazamiento
+				add $a0, $a0, $t2		#Sumamos el desplazamiento a la direccion
 
-				li $v0, 4
+				li $v0, 4			#Imprimimos el mes 
 				syscall
 						
-				li $a0, 0
+				li $a0, 0			#Imprimimos el numero del anyo
 				add $a0, $a0, $t3
 
 				li $v0, 1
 				syscall
+				
+				la $a0, B			#Imprimimos salto de linea
+				li $v0, 4
+				syscall 
 
 				li $v0, 10			#Terminamos
 				syscall
 
-Imprimir:
-
-
-Erraaaa:
-				la $a0, Err1
-				li $v0,4
-				syscall
-				j __main
-
-ErrAlg:
-				la $a0, Ealg
-				li $v0,4
-				syscall
-				j __main
-
 ErrF:
-				la $a0, Efma
-				li $v0, 4
+				la $a0, Efma			#Se ha producido un error con el formato de fecha introducido
+				li $v0, 4			#Imprimimos el mensaje de error
 				syscall
-				j __main
+				j __main			#Volvemos a empezar
 
 ErrM:
-				la $a0, Emes			#Imprimimos mensaje de error
-				li $v0, 4
-				syscall
-				j __main
+				la $a0, Emes			#Se ha producido un error con el mes introducido
+				li $v0, 4			#Imprimimos mensaje de error
+				syscall	
+				j __main			#Volvemos a empezar
 ErrD:
-				la $a0, Edia			#Imprimimos mensaje de error
-				li $v0, 4
+				la $a0, Edia			#Se ha producido un error con el dia introducido
+				li $v0, 4			#Imprimimos mensaje de error
 				syscall
-				j __main
+				j __main			#Volvemos a empezar
 ErrA:
-				la $a0, Eano			#Imprimimos mensaje de error
-				li $v0, 4
+				la $a0, Eano			#Se ha producido un error con el año introducido
+				li $v0, 4			#Imprimimos mensaje de error
 				syscall
-				j __main
+				j __main			#Volvemos a empezar
 ErrC:
-				la $a0, Eca			#Imprimimos mensaje de error
-				li $v0, 4
+				la $a0, Eca			#Se ha encontrado un caracter no permitido en la fecha
+				li $v0, 4			#Imprimimos mensaje de error
 				syscall
-				j __main
+				j __main			#Volvemos a empezar
 
 Leer:					
-				li $t1, 0
+				li $t1, 0			#Dejamos los temporales de dia mes y año a 0
 				li $t2, 0
 				li $t3, 0
 Dia:
-				addi $t4, $t4, 1
+				addi $t4, $t4, 1		#Incrementamos en 1 el contador del bucle
 				lb $t0, 0($s0)			#Cargamos el caracter de la cadena
 				beq $t0, 47, Mes		#Si encontramos un / pasamos a leer el mes
-				bge $t4, 3, ErrorFormato
+				bge $t4, 3, ErrorFormato	#Si el contador es >= 3 y no se encuentra / hay error en el formato
 				addi $t0, $t0, -48		#Restamos 48 para obtener el numero del ascii
-				bltz $t0, ErrorCaracter
+				bltz $t0, ErrorCaracter		#Si el codigo ascii es <0 o >9 hay un caracter no valido
 				bgt $t0, 9, ErrorCaracter
 				mul $t1, $t1, 10		# x10 para ir calculando las decenas
-				add $t1, $t1, $t0
+				add $t1, $t1, $t0		
 				addi $s0, $s0, 1		#Sumamos 1 a la direccion de la cadena para leer el siguiente byte
-				j Dia			#Repetimos el bucle
+				j Dia				#Repetimos el bucle
 
 Mes:
-				addi $t6 $t6, 1
+				addi $t6 $t6, 1			#Incrementamos en 1 el contador del bucle
 				addi $s0, $s0, 1		#Sumamos 1 a la direccion de la cadena para leer el siguiente byte
 				lb $t0, 0($s0)			#Cargamos el caracter de la cadena
 				beq $t0, 47, Year		#Si encontramos un / pasamos a leer el anyo
-				bge $t6, 3, ErrorFormato
+				bge $t6, 3, ErrorFormato	#Si el contador es >= 3 y no se encuentra / hay error en el formato
 				addi $t0, $t0, -48		#Restamos 48 para obtener el numero del ascii
-				bltz $t0, ErrorCaracter
+				bltz $t0, ErrorCaracter		#Si el codigo ascii es <0 o >9 hay un caracter no valido
 				bgt $t0, 9, ErrorCaracter
 				mul $t2, $t2, 10		# x10 para ir calculando las decenas
-				add $t2, $t2, $t0
+				add $t2, $t2, $t0		
 				j Mes				#Repetimpos el bucle
 
 Year:
@@ -206,88 +191,87 @@ Year:
 
 Comprobar:
 
-				bgt $t2, 12, ErrorMes
+				bgt $t2, 12, ErrorMes		#Si mes > 12, vamos a ErrorMes
 
-				beq $t2, 1, Mes31 #Enero
-				beq $t2, 2, Febrero #Febrero
-				beq $t2, 3, Mes31 #Marzo
-				beq $t2, 4, Mes30 #Abril
-				beq $t2, 5, Mes31 #Mayo
-				beq $t2, 6, Mes30 #Junio
-				beq $t2, 7, Mes31 #Julio
-				beq $t2, 8, Mes31 #Agosto
-				beq $t2, 9, Mes30 #Septiembre
-				beq $t2, 10, Mes31 #Octubre
-				beq $t2, 11, Mes30 #Noviembre
-				beq $t2, 12, Mes31 #Diciembre
+				beq $t2, 1, Mes31 		#Enero
+				beq $t2, 2, Febrero 		#Febrero
+				beq $t2, 3, Mes31 		#Marzo
+				beq $t2, 4, Mes30 		#Abril
+				beq $t2, 5, Mes31 		#Mayo
+				beq $t2, 6, Mes30 		#Junio
+				beq $t2, 7, Mes31 		#Julio
+				beq $t2, 8, Mes31 		#Agosto
+				beq $t2, 9, Mes30 		#Septiembre
+				beq $t2, 10, Mes31		#Octubre
+				beq $t2, 11, Mes30 		#Noviembre
+				beq $t2, 12, Mes31 		#Diciembre
 				
 Mes31:
-				addi $t5, $zero, 32
-				blt $t1, $t5, Algoritmo
-				j ErrorDia
+				addi $t5, $zero, 32		
+				blt $t1, $t5, Algoritmo		#Si el dia es <32 vamos a Algoritmo
+				j ErrorDia			#Si no vamos a ErrorDia
 
 Mes30:
 				addi $t5, $zero, 31
-				blt $t1, $t5, Algoritmo
-				j ErrorDia
+				blt $t1, $t5, Algoritmo		#Si el dia es <31 vamos a algoritmo
+				j ErrorDia			#Si no vamos a ErrrorDia
 
 Febrero:
-				addi $t5, $zero, 29
-				beq $t1, 29, Bisiesto
-				blt $t1,$t5, Algoritmo
-				j ErrorDia
-
+				addi $t5, $zero, 29		
+				beq $t1, 29, Bisiesto		#Si es 29 vamos a Bisiesto
+				blt $t1,$t5, Algoritmo		#Si es <29 vamos a Algoritmo
+				j ErrorDia			#Si no vamos a ErrorDia
+			
 Bisiesto:
-				#Entre 4 pero no entre 100
-				#entre 400
+								#Entre 4 pero no entre 100
+								#entre 400
 				addi $t5, $zero, 400
-				div $t3, $t5
-				mfhi $t5 #guardamos el resto en t5
-				beq $t5, 0, Algoritmo
+				div $t3, $t5			# Año/400
+				mfhi $t5 			#Guardamos el resto en t5
+				beq $t5, 0, Algoritmo		#Si el resto es 0 vamos a Algoritmo
 
 				addi $t5, $zero, 4
-				div $t3, $t5
-				mfhi $t5 #guardamos el resto en t5
-				bne $t5, 0, ErrorDia
+				div $t3, $t5			#Año /4
+				mfhi $t5 			#Guardamos el resto en t5
+				bne $t5, 0, ErrorDia		#Si el resto es != 0 vamos a ErrorDia
 
-				addi $t5, $zero, 100
-				div $t3, $t5
-				mfhi $t5 #guardamos el resto en t5
-				beq $t5, 0, ErrorDia
+				addi $t5, $zero, 100		
+				div $t3, $t5			#Año /100
+				mfhi $t5 			#Guardamos el resto en t5
+				beq $t5, 0, ErrorDia		#Si el resto es == 0 vamos a ErrorDia
 
-				j Algoritmo
+				j Algoritmo			#Si no vamos a Algoritmo
 
 Algoritmo:
-				#aplicamos la congruencia de zeller
-				ble $t2, 2, Ajuste
+								#Aplicamos la congruencia de zeller
+				ble $t2, 2, Ajuste		#Para los meses 1 y 2, vamos a Ajuste
 
-				li $t6, 12
-
+				li $t6, 12			
 				addi $t7, $t2, -14
 				div $t7, $t6
 
-				mflo $t7		# a = (14 -mes) / 12
+				mflo $t7			#a = (14 -mes) / 12
 
-				sub $t8, $t3, $t7 	# y = anyo -a
+				sub $t8, $t3, $t7 		#y = anyo -a
 
-				mul $t7, $t7, 12 	# a = 12 *a
-				add $t7, $t2, $t7	# a = a +mes
-				addi $t7, $t7, -2	# m= mes +12*a-2
+				mul $t7, $t7, 12 		# a = 12 *a
+				add $t7, $t2, $t7		# a = a +mes
+				addi $t7, $t7, -2		# m= mes +12*a-2
 
 
 				mul $t7, $t7 31
 				div $t7, $t6
-				mflo $t7		# m = 31*m /12
+				mflo $t7			# m = 31*m /12
 
 				li $t6, 4
 
 				div $t8, $t6
-				mflo $t9		# resultado = y/4
+				mflo $t9			#resultado = y/4
 
-				add $t9, $t1, $t9	# resultado = y/4 + dia
-				add $t9, $t8, $t9	# resultado = y/4 + dia + y
+				add $t9, $t1, $t9		#resultado = y/4 + dia
+				add $t9, $t8, $t9		#resultado = y/4 + dia + y
 
-				add $t9, $t9, $t7	# resultado = y/4 + dia + y + 31*m /12
+				add $t9, $t9, $t7		#resultado = y/4 + dia + y + 31*m /12
 
 				li $t6, 100
 
@@ -301,44 +285,44 @@ Algoritmo:
 				div $t8, $t6
 				mflo $t7
 
-				add $t9, $t9, $t7	# resultado = y/4 + dia + y + 31*m /12 - y/100 + y/400
+				add $t9, $t9, $t7		#resultado = y/4 + dia + y + 31*m /12 - y/100 + y/400
 
 				li $t6, 7
 				div $t9, $t6
 				mfhi $t9
 
-				li $v0, 0
-				add $v0, $v0, $t9
+				li $v0, 0			#Devolvemos el dia de la semana como un numero, siendo el Domingo 0,
+				add $v0, $v0, $t9		#el Lunes 1, Martes 2, ...
 
-				j Salir
+				j Salir				#Vamos a salir
 
-Ajuste:
-				addi $t2, $t2, 12	#mes +12
-				addi $t3, $t3, -1	#anyo -1
-				j Algoritmo
+Ajuste:								#Realizamos el ajuste necesario para los meses 1 y 2
+				addi $t2, $t2, 12		#mes +12
+				addi $t3, $t3, -1		#anyo -1
+				j Algoritmo			#Volvemos a Algoritmo
 
 ErrorMes:
-				li $v0, -1
-				j Salir
+				li $v0, -1			#Salimos de la funcion con el codigo de error -1
+				j Salir				#Mes incorrecto
 
 ErrorDia:
-				li $v0, -2
-				j Salir
+				li $v0, -2			#Salimos de la funcion con el codigo de error -2
+				j Salir				#Dia incorrecto
 
 Erroranyo:
-				li $v0, -3
-				j Salir
+				li $v0, -3			#Salimos de la funcion con el codigo de error -3
+				j Salir				#Anyo incorrecto
 
 ErrorCaracter:
-				li $v0, -4
-				j Salir
+				li $v0, -4			#Salimos de la funcion con el codigo de error -4
+				j Salir				#Caractern no valido
 
 ErrorFormato:
-				li $v0, -5
-				j Salir
+				li $v0, -5			#Salimos de la funcion con el codigo de error -5
+				j Salir				#Formato no valido
 
 Salir:								
-				li $t4, 0
+				li $t4, 0			#Dejamos a 0 los resgistros de los contadores
 				li $t5, 0
 				li $t6, 0
 				jr $ra
